9781ee298c3bef20dd40bc8fd91695ed0fcf91a8
zerahs <sebastien.zerah@gmail.com>
2018-04-19 17:17:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+cumul_antithrombotique
+======================
+
+A Symfony project created on April 19, 2018, 2:00 pm.
---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+require('../css/app.scss');
+var $ = require('jquery');
+require('bootstrap-sass');
+
+// Test javascript module - remove later
+var greet = require('./greet');
+
+
+$(document).ready(function() {
+    // $('body').prepend('<h1>'+greet('john')+'</h1>');
+    // $('[data-toggle="popover"]').popover();
+});
+
